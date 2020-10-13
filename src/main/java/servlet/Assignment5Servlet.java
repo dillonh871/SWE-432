559717f6ad919aa91479f86a7b1e19e0380b6389
dillon hoang <dillonh871@gmail.com>
2020-10-13 14:08:58 -0400

    more text

--- src/main/java/servlet/Assignment5Servlet.java
+++ src/main/java/servlet/Assignment5Servlet.java
@@ -96,7 +96,7 @@ public class Assignment5Servlet extends HttpServlet {
         out.println("<h3>Instructions</h3>");
         out.println("<p>1.For the Expression section, enter the expression that you would like to get the results of. You may name the boolean variables whatever you want and there is no limit on the number  boolean variables you want to use. Press on the button for the operator that you would like to use. You may also type the operator as well.");
         out.println("<br>");
-        out.println("(Note that &&, ||, and ! are used for AND,OR and NOT and are the only available operators)");
+        out.println("(Note that &&, ||, ^ are used for AND,OR and EXCLUSIVE OR and you may type whatever your choices. We also support multiple syntaxes such as &, AND, and, or, OR, etc.)");
         out.println("</p>");
         out.println("<p>2.For the Boolean Var(s) section, enter all the boolean variables you used and seperate them with a semicolon.");
         out.println("<br>");
@@ -104,7 +104,7 @@ public class Assignment5Servlet extends HttpServlet {
         out.println("</p>");
         out.println("<p>3.For the Value(s) section, enter values for the boolean variables and seperate them with a semicolon.Make sure to enter the values in the order corresponding to the boolean variable you want it for");
         out.println("<br>");
-        out.println("For example, if the user has boolean variables \"tomorrow;today\" you can enter \"0;1\"");
+        out.println("For example, if the user has boolean variables \"tomorrow;today\" you can enter \"0;1\", \"T;F\", \"t;f\", \"true;false\", etc.");
         out.println("</p>");
         out.println("<p>4.Press the submit button when you are done entering your expression and variables</p>");
 
@@ -175,6 +175,8 @@ public class Assignment5Servlet extends HttpServlet {
         out.println("<body>");
         out.println("<h1 align=center>SWE 432 Assignment 5.</h1>");
         out.println("<h2 align=center>Long Hoang and Faiz Zia</h2>");
+        out.println("<h1 >PredicateTruth Table</h1>");
+        out.println("This is your Predicate Truth Table based on the Expression(s), Variables, and Values you chose");
         out.println("</body>");
     }
 
