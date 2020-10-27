2dad1fd74cfd8855fb3a34ac821f8e4bef2d920f
dillon hoang <dillonh871@gmail.com>
2020-10-27 14:13:48 -0400

    new changes assign 7

--- src/main/java/servlet/Assignment5Servlet.java
+++ src/main/java/servlet/Assignment5Servlet.java
@@ -572,7 +572,7 @@ public class Assignment5Servlet extends HttpServlet {
         out.println("	</table>");
         out.println("	<input type=\"submit\" value=\"Submit\" name=\"submitBtn\" align=\"center\">");
         out.println("   <input type=\"submit\" value=\"Save\" name=\"saveBtn\" align=\"center\">");
-        out.println("   <input type=\"submit\" value=\"Go to saved Predicates\" name=\"persistBtn\" align=\"center\">");
+        out.println("   <input type=\"button\" value=\"Go to saved Predicates\" name=\"persistBtn\" align=\"center\">");
         out.println("</form>");
         out.println("<h2 align=\"center\">Collaboration Summary</h2>");
         out.println("<p>For this assignment, Long and Faiz collaborated on implementation ideas and how we thought the design for the Post web page should look like. We also both took some time going over how the algorithm for solving the predicates should be like. And afterwards Long and Faiz split up the work so we could accomplish the tasks in an efficient manner. Long started the assignment by setting up the servlet so both of us had something to work with. Long then worked on converting the flat HTML file we created into the doGet method and setting up all the print methods such as PrintHead, PrintBody, PrintTail, and PrintPostBody. Faiz then worked on converting our algorithm into the the doPost method and creating the table for the truth table. We then took some time afterwards to look over one another's work and debug. This assignment gave us a lot of exposure on how to work with java servlets and the methods within them such as doGet and doPost.");
