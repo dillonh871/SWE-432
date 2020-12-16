ff9b77fb2136c939980958b6618fb2632863fe19
dillon hoang <dillonh871@gmail.com>
2020-12-15 19:03:49 -0500

    added finalExam

--- src/main/java/servlet/finalExam.java
+++ src/main/java/servlet/finalExam.java
@@ -71,7 +71,7 @@ private void PrintHead (PrintWriter out)
 
    out.println("  <style>.container{display: flex;justify-content: center;}</style>");
    out.println("  <style>.sub-container{display: flex;flex-direction: row;align-items: center;justify-content: space-evenly;}</style>");
-   out.println("  <style>.buttons-container{display: \"flex\"; flex-Direction: \"column\";align-items: \"center\";justify-content: \"center\";}</style>");
+   out.println("  <style>.buttons-container{display: flex; flex-Direction: column;align-items: center;justify-content: center;}</style>");
    out.println("  <style>.textbox{align-items: center;display: flex;flex-direction: column;width: 300px;height: 450px;}</style>");
 
    out.println("  <style>.box-title{text-align: center;}</style>");
