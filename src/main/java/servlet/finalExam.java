974e47bca38bd2aef5e58fde248ed3e4f5c1eefe
dillon hoang <dillonh871@gmail.com>
2020-12-15 19:31:07 -0500

    added finalExam

--- src/main/java/servlet/finalExam.java
+++ src/main/java/servlet/finalExam.java
@@ -117,7 +117,7 @@ private void PrintBody (PrintWriter out, String result)
 
     //buttons
     out.println("       <div class=\"buttons-container\">");
-    out.println("           <label class=\"block\"> <input type=\"radio\" /> Ascending </label>");
+    out.println("           <label class=\"block\"> <input type=\"radio\" />   Ascending </label>");
     out.println("           <label class=\"block\"> <input type=\"radio\" /> Descending </label>");
     out.println("           <button> Submit </button>");
     out.println("       </div>");
